3548e9e56ca3946b2a6350a802465b86c6d9d58d
den-taku <dentaku1412@gmail.com>
2023-04-11 17:44:39 +0900

    Implement fundamental methods

---
+++ src/graph.rs
@@ -0,0 +1,79 @@
+use rand::Rng;
+use std::cmp::{max, min};
+use std::collections::HashSet;
+use std::convert::identity;
+
+/// generate sinple undirected graph
+pub fn generate_random_graph_edges(nodes: usize, edges: usize) -> HashSet<(usize, usize)> {
+    if nodes == 0 {
+        // empty graph
+        return HashSet::new();
+    } else if edges >= nodes * (nodes - 1) / 2 {
+        // complete graph
+        let mut all = HashSet::new();
+        for u in 0..nodes {
+            for v in u + 1..nodes {
+                all.insert((u, v));
+            }
+        }
+        return all;
+    }
+    let mut ret = HashSet::with_capacity(edges);
+
+    let mut rng = rand::thread_rng();
+    while ret.len() < edges {
+        let u = rng.gen::<usize>() % nodes;
+        let v = rng.gen::<usize>() % nodes;
+        if u != v {
+            let edge = (min(u, v), max(u, v));
+            ret.insert(edge);
+        }
+    }
+    ret
+}
+
+/// Adjacent matrix of undirected graph
+#[derive(Clone, Debug)]
+pub struct Graph(Vec<Vec<usize>>);
+
+impl Graph {
+    pub fn new<T: std::iter::IntoIterator<Item = (usize, usize)>>(nodes: usize, edges: T) -> Self {
+        let mut adjacent = vec![Vec::new(); nodes];
+        for (u, v) in edges {
+            // undirected
+            adjacent[u].push(v);
+            adjacent[v].push(u);
+        }
+        Self(adjacent)
+    }
+
+    pub fn is_connected(&self) -> bool {
+        if self.0.is_empty() {
+            return true;
+        }
+        let mut visited = vec![false; self.0.len()];
+        visited[0] = true;
+        self.dfs(0, &mut visited, &|_| {}, &|_| {});
+        visited.into_iter().all(identity)
+    }
+
+    /// BFS
+    ///
+    /// - condition: before visiting `start`, you should make the start `true`
+    pub fn dfs<F: Fn(usize), U: Fn(usize)>(
+        &self,
+        start: usize,
+        visited: &mut [bool],
+        before_visited: &F,
+        after_visited: &U,
+    ) {
+        before_visited(start);
+        for &v in &self.0[start] {
+            if !visited[v] {
+                visited[v] = true;
+                self.dfs(v, visited, before_visited, after_visited);
+            }
+        }
+        after_visited(start);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use clap::Parser;
+use graph::*;
+use itertools::Itertools;
+
+pub mod graph;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The number of nodes
+    #[arg(short, long, default_value_t = 1)]
+    nodes: usize,
+
+    /// The number of edges
+    #[arg(short, long, default_value_t = 0)]
+    edges: usize,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    while 'condition: {
+        let edges = generate_random_graph_edges(args.nodes, args.edges);
+        println!("{edges:?}");
+        for (i, sub) in edges.clone().into_iter().permutations(args.edges - 3).enumerate() {
+            println!("{i}");
+            if !Graph::new(args.nodes, sub).is_connected() {
+                break 'condition true
+            }
+        }
+        println!("{edges:?}");
+        false
+    } {}
+}
